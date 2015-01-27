f5634aa21211617dad0ebbf3ae85d7c8ae48d67c
Josh Cheng <joshwalker.cheng@gmail.com>
2015-01-27 18:56:58 +0800

    First working build

--- src/java/main/com/mozilla/dashboard/dashboard.java
+++ src/java/main/com/mozilla/dashboard/dashboard.java
@@ -10,32 +10,36 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+
 public class dashboard 
 {
-    private static int      DISPLAY_NUMBER  = 99;
-    private static String   XVFB            = "/usr/bin/Xvfb";
-    private static String   XVFB_COMMAND    = XVFB + " :" + DISPLAY_NUMBER;
     private static String   URL             = "http://kaichih.github.io/FxOS-Dashboard/";
-    private static String   RESULT_FILENAME = "/Users/Joshwalker/Google 雲端硬碟/Mozilla/screenshot.png";
-
+    //private static String   URL             = "http://www.yahoo.com";
+    private static String   RESULT_FILENAME = "C:\\Users\\user\\Dropbox\\Mozilla\\DBscreenshot.png";
+    private static int   Timeout = 300;
+    
     public static void main ( String[] args ) throws IOException, InterruptedException
     {
-    	
-    	
-        //Process p = Runtime.getRuntime().exec(XVFB_COMMAND);
         FirefoxBinary firefox = new FirefoxBinary();
-        //firefox.setEnvironmentProperty("DISPLAY", ":" + DISPLAY_NUMBER);
-        WebDriver driver = new FirefoxDriver(firefox, null);
-        driver.manage().window().setSize(new Dimension(320, 480));
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().window().setSize(new Dimension(480, 640));
         driver.get(URL);
-        Thread.sleep(10000);
+        System.out.println("URL Opened:"+URL );
+        
+        while(true)
+        {
+        System.out.println("Sleep for "+Timeout+" Seconds");
+        Thread.sleep(Timeout*1000);
+        System.out.println("Taking screenshot");
         
         File scrFile = ( (TakesScreenshot) driver ).getScreenshotAs(OutputType.FILE);
         FileUtils.copyFile(scrFile, new File(RESULT_FILENAME));
-        driver.close();
-        //p.destroy();
+        System.out.println("Refresh page");
+        driver.navigate().refresh();
+        }
     }
 }
